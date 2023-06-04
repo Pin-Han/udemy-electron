3783eb679cbd8114bdfad9136ccf65e9168736bb
Patrick <patrick04184@gmail.com>
2023-06-04 16:13:24 +0800

    lesson 3- 10

--- main.js
+++ main.js
@@ -1,72 +1,99 @@
 // Modules
-const {app, BrowserWindow, session} = require('electron')
+const { app, BrowserWindow } = require("electron");
+
+setTimeout(() => {
+  console.log("Checking ready: " + app.isReady());
+}, 2000);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow, secWindow
+let mainWindow, secondaryWindow;
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
-
+function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
+    width: 1000,
+    height: 800,
     webPreferences: {
       // --- !! IMPORTANT !! ---
       // Disable 'contextIsolation' to allow 'nodeIntegration'
       // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
-      nodeIntegration: true
-    }
-  })
-  secWindow = new BrowserWindow({
-    width: 800, height: 600,
-    x: 200, y: 200,
+      nodeIntegration: true,
+    },
+    // show: false,
+    backgroundColor: "#24ceb9",
+  });
+  secondaryWindow = new BrowserWindow({
+    width: 600,
+    height: 300,
     webPreferences: {
-      webPreferences: {
-        // --- !! IMPORTANT !! ---
-        // Disable 'contextIsolation' to allow 'nodeIntegration'
-        // 'contextIsolation' defaults to "true" as from Electron v12
-        contextIsolation: false,
-        nodeIntegration: true
-      },
-      partition: 'persist:part1'
-    }
-  })
-
-  let ses = mainWindow.webContents.session
-  let ses2 = secWindow.webContents.session
-  let defaultSes = session.defaultSession
-
-  ses.clearStorageData()
-
-  // console.log( Object.is(ses, customSes) )
-
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+    backgroundColor: "#24ceb9",
+    show: false,
+    parent: mainWindow,
+    modal: true,
+  });
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
-  secWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
+  secondaryWindow.loadFile("secondary.html");
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
-  secWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+
+  setTimeout(() => {
+    secondaryWindow.show();
+    setTimeout(() => {
+      secondaryWindow.close();
+      secondaryWindow = null;
+    }, 3000);
+  }, 2000);
+
+  // secondaryWindow.once("ready-to-show", secondaryWindow.show);
 
   // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
-  secWindow.on('closed',  () => {
-    secWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+  // secondaryWindow.on("closed", () => {
+  //   secondaryWindow = null;
+  // });
 }
 
+app.on("browser-window-blur", (e) => {
+  console.log("App unfocus");
+  // setTimeout(() => {
+  //   app.quit();
+  // }, 3000);
+});
+
+// app.on("browser-window-focus", (e) => {
+//   console.log("App focused");
+// });
+
+app.on("before-quit", (e) => {
+  console.log("Preventing app from quitting");
+  e.preventDefault();
+});
+
 // Electron `app` is ready
-app.on('ready', createWindow)
+app.on("ready", () => {
+  console.log(app.getPath("desktop"));
+  console.log(app.getPath("music"));
+  console.log(app.getPath("temp"));
+  console.log(app.getPath("userData"));
+  console.log("App is ready");
+  createWindow();
+});
 
 // Quit when all windows are closed - (Not macOS - Darwin)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on('activate', () => {
-  if (mainWindow === null) createWindow()
-})
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
